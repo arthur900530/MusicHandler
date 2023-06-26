1a0b70de31ff1c5a1c5977c29d750f81eebc14c1
Arthur <moneychien20639@gmail.com>
2023-06-26 15:26:31 +0800

    modified attr release date

--- src/music_handler.py
+++ src/music_handler.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 class Music():
     def __init__(self, name, artist, realease_date) -> None:
         self.name = name
         self.artist = artist
-        self.release_date = realease_date
+        self.release_date = datetime.strptime(realease_date,'%Y-%m-%d')
